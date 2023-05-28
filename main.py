4730fa43d45adc3078a9562304f19d78f0b107e1
marilia91 <marilia.iza112@gmail.com>
2023-05-28 14:37:36 -0300

    palindromos resolvido

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 """Main functions"""
 
+import re
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    string = string.lower().replace(" ", "")
+    string = re.sub(r'[^\w\s]','',string)
+    string_inversa = string[::-1]
+
+    if string == string_inversa:
+        return True
+    else:
+        return False
+
+#texto = input(str("Digite uma string: "))
+
+"""def is_palindrome(string: str) -> bool:
+    string = string.strip().lower().isalnum()
+    letras = string.split()
+    letras_juntas = ''.join(letras)
+    if letras_juntas == letras_juntas[::-1]:
+        return True
+    else:
+        return False
+    
+    Check if string is palindrome."""
+    
+    #return False
+
+
+
+
+
